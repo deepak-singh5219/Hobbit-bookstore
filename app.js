e034fa9803f6ee5c9f69d9033312cfbb177e33c1
Deepak Singh <singhdeepak5219@gmail.com>
2022-12-04 17:41:45 +0530

    added middlewares: express and morgan

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const morgan = require('morgan');
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+app.use(cookieParser());
+app.use(morgan('tiny')); // morgan loader
 
 module.exports = app;
