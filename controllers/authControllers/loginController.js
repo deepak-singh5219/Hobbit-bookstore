14f3630bd3baa6ca880bb6e4360d8b306d52206e
Deepak Singh <singhdeepak5219@gmail.com>
2022-12-04 18:35:08 +0530

    login controller made and added in auth controller

---
+++ controllers/authControllers/loginController.js
@@ -0,0 +1,44 @@
+const User = require('.../models/userModel');
+const asyncHandler = require('.../services/asyncHandler');
+const CustomError = require('.../utils/customError');
+const cookieOptions = require('.../utils/cookieOptions');
+
+/******************************************************
+ * @LOGIN
+ * @route http://localhost:4000/api/auth/login
+ * @description User signIn Controller for login new user
+ * @parameters  email, password
+ * @returns User Object
+ ******************************************************/
+
+const login = asyncHandler(async (req,res) => {
+    // login Controller
+    const { email,password } = req.body;
+    // valildate if email and password are present
+     
+    if(!(email && password)) {
+        throw new CustomError('Email and Password are required', 400);
+    }
+
+    const user = User.findOne({email}).select("+password");
+    if(!user){
+        throw new CustomError('Invalid Credentials', 400);
+    }
+
+    const isPasswordMatched = await user.comparePassword(password);
+    
+    if(isPasswordMatched){
+        const token = user.genJwtToken();
+        user.password = undefined;
+        res.cookie("token",token, cookieOptions);
+        return res.status(200).json({
+            success:true,
+            token,
+            user
+        })
+    }
+
+    throw new CustomError('Invalid crendentials',400);
+})
+
+module.exports = login;
