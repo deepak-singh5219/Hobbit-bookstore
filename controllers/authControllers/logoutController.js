d724b6b557adf704e6b19b79f3affaba01f83342
Deepak Singh <singhdeepak5219@gmail.com>
2022-12-05 22:03:45 +0530

    Logout controller added in models

---
+++ controllers/authControllers/logoutController.js
@@ -0,0 +1,27 @@
+const User = require('.../models/userModel');
+const asyncHandler = require('.../services/asyncHandler');
+const CustomError = require('.../utils/customError');
+const cookieOptions = require('.../utils/cookieOptions');
+
+/******************************************************
+ * @LOGOUT
+ * @route http://localhost:4000/api/auth/logout
+ * @description User logout bby clearing user cookies
+ * @parameters  
+ * @returns success message
+ ******************************************************/
+
+const logout = asyncHandler(async (req,res) => {
+    
+    res.cookie("token",null, {
+       expires: new Date(Date.now()),
+       httpOnly: true     
+    })
+
+    res.status(200).json({
+        success: true,
+        message:"Logged Out"
+    })
+})
+
+module.exports = logout;
