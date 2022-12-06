adb947447a9c5e6cc9ce79152b3300cafe27c026
Deepak Singh <singhdeepak5219@gmail.com>
2022-12-07 02:44:24 +0530

    getProfile controller added to authControllers

---
+++ controllers/authControllers/profileController.js
@@ -0,0 +1,27 @@
+const User = require(".../models/userModel");
+const asyncHandler = require(".../services/asyncHandler");
+const CustomError = require(".../utils/customError");
+const cookieOptions = require(".../utils/cookieOptions");
+
+/******************************************************
+ * @GET_PROFILE
+ * @REQUEST_TYPE GET
+ * @route http://localhost:4000/api/auth/profile
+ * @description check for token and populate req.user
+ * @parameters
+ * @returns User Object
+ ******************************************************/
+
+const getProfile = asyncHandler(async (req, res) => {
+  const { user } = req;
+  if (!user) {
+    throw new CustomError("User not found", 404);
+  }
+
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
+module.exports = getProfile;
