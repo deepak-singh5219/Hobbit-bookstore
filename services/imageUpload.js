1e0dd1e1c523a77d9f6bd322122fc5ed1665a84a
Deepak Singh <singhdeepak5219@gmail.com>
2023-01-02 18:42:44 +0530

    imageUpload service

--- services/imageUpload.js
+++ services/imageUpload.js
@@ -11,13 +11,5 @@ const s3FileUpload = asyncHandler(async({bucketName, key, body, contentType}) =>
     .promise()
 })
 
-const s3DeleteFile = asyncHandler(async({bucketName, key}) => {
-
-    return await s3.deleteObject({
-        Bucket: bucketName,
-        Key: key
-    })
-    .promise()
-})
 
 module.exports = {s3FileUpload,s3DeleteFile};
